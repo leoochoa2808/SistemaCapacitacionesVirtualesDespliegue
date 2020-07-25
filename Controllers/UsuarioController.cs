7e70c92240562ff857b39b11a65cc78ba10639c3
Josselyn Flores <josselyn_flores@usmp.pe>
2020-07-24 23:57:31 -0500

    Registrar Participante

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -10,10 +10,29 @@ using Microsoft.EntityFrameworkCore;
 namespace Sistema_de_Capacitaciones_Virtuales.Controllers
 {
     public class UsuarioController : Controller
-    {          
+    {       
+        private readonly KleerDbContext _context;
+        public UsuarioController (KleerDbContext context) {
+            _context = context;
+        }   
         public IActionResult Login()
         {
             return View();
+        }
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registrar(Participante p)
+        {
+         if (ModelState.IsValid) {
+                _context.Add (p);
+                _context.SaveChanges ();
+                return RedirectToAction ("Index", "Home");
+            } else
+        return View (p);
         }
       
     }
