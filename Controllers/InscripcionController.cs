1e8007638806df8511f25becc26b50b685d4bdb4
jose2410 <31625020+jose2410@users.noreply.github.com>
2020-08-03 18:06:39 -0500

    InscripcionController

--- Controllers/InscripcionController.cs
+++ Controllers/InscripcionController.cs
@@ -6,8 +6,6 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Net.Mail;
-using System.Net;
 using Sistema_de_Capacitaciones_Virtuales.Models;
 
 namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
@@ -21,7 +19,6 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
         //variable iduser graba la sesion del usuario
         public static int iduser;
 
-<<<<<<< HEAD
         public IActionResult PreInscripcionCursos (int? idE, int? idU) {
 
             var evento = _context.Eventos.FirstOrDefault(e =>e.Id == idE);
@@ -31,18 +28,6 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
             if (usuario != null || evento != null) {
                 int longitud = 7;
                 const string alfabeto = "0123456789"; //"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-=======
-        
-        public IActionResult PreInscripcionCursos (int? idE, int? idU) {
-
-            var evento = _context.Eventos.FirstOrDefault(e =>e.Id == idE);
-            var usuario = _context.Participantes.FirstOrDefault(p => p.Id == idU);
-            var destino_correo = usuario.Correo;
-            iduser = usuario.Id;
-            if (usuario != null && evento != null) {
-                int longitud = 7;
-                const string alfabeto =  "0123456789";//"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
->>>>>>> 6140da1236874cd6164a128fee9babddad7b8185
                 StringBuilder token = new StringBuilder ();
                 Random rnd = new Random ();
 
@@ -50,40 +35,14 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
                     int indice = rnd.Next (alfabeto.Length);
                     codigo = token.Append (alfabeto[indice]).ToString ();
                 }
-                SmtpClient smtp = new SmtpClient("stmp.gmail.com",587);
-                smtp.Credentials = new NetworkCredential("joseacb2496@gmail.com","Peluchin24-10");
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtp.EnableSsl = true;
-                smtp.UseDefaultCredentials = false;
 
-                MailMessage msg = new MailMessage ();
-
-                msg.From = new MailAddress("joseacb2496@gmail.com","Preinscripción curso - ABC");
-                msg.To.Add(new MailAddress(destino_correo));
-                msg.Subject = "Preinscripción curso - ABC";
-                msg.IsBodyHtml = true;
-                //ENVIA UNA COPIA DEL CORREO
-                msg.Bcc.Add ("joseacb2496@gmail.com");
-                //toda estructura html es texto y css
-                msg.Body =
-                    "<body>" +
-                    "<div id='msg'><p>SE HA REALIZADO SU PREINSCRIPCION CON EL SIGUIENTE CÓDIGO DE PAGO: </p><br><strong><h1>" + codigo + "</h1><strong></div><br>" +
-                    "</body>";
-
-                smtp.Send (msg);
-
-
- /*                System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage ();
+                System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage ();
 
                 msg.To.Add (destino_correo);
-<<<<<<< HEAD
                 msg.Subject = "Pre Inscripción Curso - ABC";
-=======
-                msg.Subject = "Preinscripción curso - ABC";
->>>>>>> 6140da1236874cd6164a128fee9babddad7b8185
                 msg.SubjectEncoding = System.Text.Encoding.UTF8;
                 //ENVIA UNA COPIA DEL CORREO
-                msg.Bcc.Add ("FlashGameEnterprise@gmail.com");
+                msg.Bcc.Add ("joseacb2496@gmail.com");
                 //toda estructura html es texto y css
                 msg.Body =
                     "<body>" +
@@ -92,11 +51,11 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
                     "</body>";
                 msg.BodyEncoding = System.Text.Encoding.UTF8;
                 msg.IsBodyHtml = true;
-                msg.From = new System.Net.Mail.MailAddress ("FlashGameEnterprise@gmail.com");
+                msg.From = new System.Net.Mail.MailAddress ("joseacb2496@gmail.com");
 
                 //EL CORREO QUE VA A ENVIAR EL MENSAJE DE PRUEBA
                 System.Net.Mail.SmtpClient cliente = new System.Net.Mail.SmtpClient ();
-                cliente.Credentials = new System.Net.NetworkCredential ("FlashGameEnterprise@gmail.com", "Queteimporta123");
+                cliente.Credentials = new System.Net.NetworkCredential ("joseacb2496@gmail.com", "Peluchin24-10");
                 //PUERTO QUE USA GMAIL PARA ENVIAR LOS CORREOS
                 cliente.Port = 587;
                 cliente.EnableSsl = true;
@@ -104,7 +63,6 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
                 cliente.Host = "smtp.gmail.com"; //smtp particular o institucional, unicamente que pongas correo. pero se necesitas las credenciales smpt
                 //eso se especifica en el web.config(appsettings.json) 
 
-<<<<<<< HEAD
                 cliente.Send (msg);
                 //return View();
                 
@@ -118,29 +76,11 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
                 pago.MontoPago = evento.Inversion;
                 pago.estado_pago = "Pago Pendiente";
                 pago.TipoPagoId = 1;
-=======
-                cliente.Send (msg); */
-                var p = new Pago ();
-                p.EventoId = evento.Id;
-                p.CodPago = Int32.Parse(codigo);
-                p.ParticipanteId = usuario.Id;
-                p.FechaEmision = DateTime.Now;
-                //DateTime fecha = DateTime.Now.AddDays(7);
-                p.FechaVenc = DateTime.Now.AddDays(7);
-                p.MontoPago = evento.Inversion;
-                p.estado_pago = "Pago Pendiente";
-
->>>>>>> 6140da1236874cd6164a128fee9babddad7b8185
                 //return View();
                 _context.Add(pago);
                 _context.SaveChanges();
-<<<<<<< HEAD
                 return RedirectToAction("CursoDetalle", "Curso");
             }else {
-=======
-                return RedirectToAction ("ModalPrueba", "Inscripción");
-            } else {
->>>>>>> 6140da1236874cd6164a128fee9babddad7b8185
                 TempData["Message"] = "Correo invalido";
                 //return RecuperarC ("Correo invalido"); 
                 return NotFound ();
@@ -161,12 +101,6 @@ namespace Sistema_de_Capacitaciones_Virtuales.Controllers {
             var lista = _context.Pagos.Where(u=>u.ParticipanteId == usuario.Id && u.estado_pago == "Pago Pendiente").Include(e=>e.Evento).Include(t=>t.TipoPago);
             return View();
         }
-
-        public IActionResult ModalPrueba()
-        {
-            return View();
-        }
-        
 
     }
 }
